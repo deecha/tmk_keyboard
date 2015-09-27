bdeb0fac36bbec88a2c932129f49f4f2ab3e93ae
Deepak S <dcs.galco@gmail.com>
2015-09-26 23:55:29 -0400

    Soldered Right Alt and fixed the keymap

--- keyboard/dc_101/keymap_common.h
+++ keyboard/dc_101/keymap_common.h
@@ -43,14 +43,14 @@ extern const uint16_t fn_actions[];
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2F, K2G, K2H, K2I, K2J, K2K, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3I, K3J, K3K, K3L, \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4C, K4G, K4I, K4J, K4K, \
-    K50, K51,            K56,                K58, K5C,K5F, K5G, K5H, K5I, K5K, K5L \
+    K50, K51,            K56,                K5A, K5C,K5F, K5G, K5H, K5I, K5K, K5L \
 ) { \
     { KC_##K00, KC_NO, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_NO, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E, KC_##K0F, KC_##K0G, KC_##K0H }, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_##K1L  }, \
     { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_NO, KC_NO, KC_##K2F, KC_##K2G, KC_##K2H, KC_##K2I, KC_##K2J, KC_##K2K }, \
     { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_##K3C, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_##K3I, KC_##K3J, KC_##K3K, KC_##K3L }, \
     { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_##K49, KC_##K4A, KC_NO, KC_##K4C, KC_NO, KC_NO, KC_NO, KC_##K4G, KC_NO, KC_##K4I, KC_##K4J, KC_##K4K }, \
-    { KC_##K50, KC_##K51, KC_NO, KC_NO, KC_NO, KC_NO, KC_##K56,   KC_NO,    KC_##K58, KC_NO,    KC_NO,    KC_NO,    KC_##K5C, KC_NO, KC_NO, KC_##K5F, KC_##K5G, KC_##K5H, KC_##K5I, KC_NO, KC_##K5K, KC_##K5L }  \
+    { KC_##K50, KC_##K51, KC_NO, KC_NO, KC_NO, KC_NO, KC_##K56,   KC_NO,    KC_NO, KC_NO,    KC_##K5A,    KC_NO,    KC_##K5C, KC_NO, KC_NO, KC_##K5F, KC_##K5G, KC_##K5H, KC_##K5I, KC_NO, KC_##K5K, KC_##K5L }  \
 }
 
 /* ANSI valiant. No extra keys for ISO */
